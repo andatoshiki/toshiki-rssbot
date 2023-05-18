b92d3efc5fa007a43be5280be4788e9a3c5fc71d
andatoshiki <hello@toshiki.dev>
2023-05-18 13:21:43 +0800

    fix(pkg): change github pacakge developer usernames from personal to organization wide

--- internal/preview/tgraph.go
+++ internal/preview/tgraph.go
@@ -3,8 +3,7 @@ package tgraph
 import (
 	"github.com/andatoshiki/toshiki-rssbot/internal/config"
 	"github.com/andatoshiki/toshiki-rssbot/internal/log"
-
-	"github.com/andatoshiki/telegraph-go"
+	"github.com/indes/telegraph-go"
 )
 
 var (
